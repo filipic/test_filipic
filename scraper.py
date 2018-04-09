af91147a3bec2f35c92e6427c3fdf82da69be174
ettore <ettore@depp.it>
2018-04-09 14:18:22 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -15,7 +15,7 @@ hkt = pytz.timezone('Asia/Hong_Kong')
 dt = datetime.now().replace(tzinfo=hkt).date()
 data = {titles[i]: values[i] for i in range(0, 4)}
 data['date'] = dt
-data={'nome':'gentiloni' , 'id': '1', 'tipo':'org', 'subtype':'gov'}
+data={'nome':'gentiloni' , 'given_name':'paolo', 'id': '1', 'tipo':'org', 'subtype':'gov'}
 scraperwiki.sqlite.save(unique_keys=['id'], data=data)
 data={'given_name':'orlando' , 'id': '2', 'tipo':'person', 'org_id':'1'}
 scraperwiki.sqlite.save(unique_keys=['id'], data=data)
